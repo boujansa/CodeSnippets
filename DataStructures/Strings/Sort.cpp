8ded168c660876cf429bdbdb1de9e80a2ef3b12c
Sayf Boujan <sayfboujan@Sayfs-MacBook-Pro.local>
2020-08-16 22:19:41 -0700

    adding sorting for strs

---
+++ DataStructures/Strings/Sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    return 0;
+}
+
+class Solution {
+public:
+    //sorts string in increasing order alphabetically
+    static bool sortStr(char a, char b)
+    {
+        return a < b;
+    }
+};
+
